3ba502a9a1f0cad1858fc2d8882bb896cd8faa6a
SerjP <brainditrff@gmail.com>
2022-10-18 11:51:26 +0300

    Добавьте файлы проекта.

---
+++ 18-10-2/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] A = new int[20];
+int[] B = new int[20];
+
+
+for (int i = 0; i < 20; i++)
+{
+    A[i] = random();
+}
+
+for (int i = 0; i < 20; i++)
+{
+    if (A[i] <= 888)
+    {
+        B[i] = A[i];
+    }
+}
+
+int temp;
+for (int i = 0; i < 19; i++)
+{
+    for (int j = i+1; j < 20; j++)
+    {
+        if (B[i] < B[j])
+        {
+            temp = B[i];
+            B[i] = B[j];
+            B[j] = temp;
+        }
+    }
+}
+
+for (int i = 0; i<20; i++)
+{
+    if (B[i] != 0)
+    {
+        Console.WriteLine(B[i]);
+    }
+}
+
+static int random()
+{
+    Random rnd = new Random();
+    int value = rnd.Next(-2000, 2000) ;
+    return value;
+}
